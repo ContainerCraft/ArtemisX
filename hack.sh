08a231edf820601c4dc2b2a9bd4382dc2d16b5ad
usrbinkat <usrbinkat@braincraft.io>
2021-04-13 13:48:57 -0700

    spindle tweaks

--- hack.sh
+++ hack.sh
@@ -3,9 +3,7 @@ clear && echo "
 ####################################
 # Artemis -- Minimum Viable GitOps #
 ####################################
-
 "
-
 artemisPath="$(pwd)/artemis"
 NAMESPACES="\
   argocd \
@@ -13,7 +11,6 @@ NAMESPACES="\
   kubevirt-hostpath-provisioner \
 "
 
-clear
 for namespace in ${NAMESPACES}; do
   echo
   echo ">> Deploying ${namespace}"
@@ -21,12 +18,9 @@ for namespace in ${NAMESPACES}; do
  #kubectl create --save-config -n ${namespace} -f ${artemisPath}/${namespace}
 done
 echo
-kubectl taint nodes --all node-role.kubernetes.io/master-
-kubectl label node artemis --overwrite node-role.kubernetes.io/master-
-kubectl label nodes --all node-role.kubernetes.io/worker
-kubectl label nodes --all ingress-ready=true
-
-
+kubectl label nodes --overwrite --all node-role.kubernetes.io/worker=""
+kubectl taint nodes --overwrite --all node-role.kubernetes.io/master-
+kubectl label nodes --overwrite --all ingress-ready=true
 
 # kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
 # kubectl create --save-config -n ingress-nginx -f artemis/ingress-nginx/manifest.yml 
